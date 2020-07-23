7b3402cf126a4222434c7a172a9b564c2a39d0fe
ggguille <999uille@gmail.com>
2020-07-23 22:53:24 +0200

    Build UI and init dynamic dice numbers

--- lib/main.dart
+++ lib/main.dart
@@ -16,8 +16,33 @@ void main() {
 }
 
 class DicePage extends StatelessWidget {
+
+  int _leftDiceNumber = 5;
+  int _rightDiceNumber = 1;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                print('Left button got pressed');
+              },
+              child: Image.asset('images/dice$_leftDiceNumber.png'),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                print('Right button got pressed');
+              },
+              child: Image.asset('images/dice$_rightDiceNumber.png'),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
